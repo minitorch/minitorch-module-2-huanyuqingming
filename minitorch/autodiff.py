9471b156cb464417439f2e43826f6e874c6173f5
huanyuqingming <huanyuqingming@sjtu.edu.cn>
2024-11-03 15:20:13 +0800

    finish module 2

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Any, Iterable, List, Tuple
 
-from typing_extensions import Protocol
+from typing_extensions import Protocol, runtime_checkable
 
 # ## Task 1.1
 # Central Difference calculation
@@ -22,12 +22,19 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    # TODO: Implement for Task 1.1.
+    # raise NotImplementedError('Need to implement for Task 1.1')
+    vals1 = list(vals)
+    vals2 = list(vals)
+    vals1[arg] = vals1[arg] + epsilon
+    vals2[arg] = vals2[arg] - epsilon
+    return (f(*vals1) - f(*vals2)) / (2 * epsilon)
 
 
 variable_count = 1
 
 
+@runtime_checkable
 class Variable(Protocol):
     def accumulate_derivative(self, x: Any) -> None:
         pass
@@ -60,7 +67,27 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     Returns:
         Non-constant Variables in topological order starting from the right.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    # TODO: Implement for Task 1.4.
+    # raise NotImplementedError('Need to implement for Task 1.4')
+    sorted_vars = []
+    visited = set()
+
+    def is_constant(val):
+        return not isinstance(val, Variable) or val.history is None
+
+    def visit(var: Variable):
+        if is_constant(var):
+            return
+
+        var_id = var.unique_id
+        if var_id not in visited:
+            visited.add(var_id)
+            for parent in var.parents:
+                visit(parent)
+            sorted_vars.append(var)
+
+    visit(variable)
+    return reversed(sorted_vars)
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -74,8 +101,26 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
 
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
-    raise NotImplementedError("Need to include this file from past assignment.")
+    # TODO: Implement for Task 1.4.
+    # raise NotImplementedError('Need to implement for Task 1.4')
+    # 计算拓扑排序
+    sorted_vars = topological_sort(variable)
 
+    # 创建一个字典来存储每个变量的导数
+    derivatives = {variable.unique_id: deriv}
+
+    # 按照拓扑排序的顺序进行反向传播
+    for var in sorted_vars:
+        if var.unique_id in derivatives:
+            d_output = derivatives[var.unique_id]
+            if var.is_leaf():
+                var.accumulate_derivative(d_output)
+            else:
+                for parent, d_parent in var.chain_rule(d_output):
+                    if parent.unique_id in derivatives:
+                        derivatives[parent.unique_id] += d_parent
+                    else:
+                        derivatives[parent.unique_id] = d_parent
 
 @dataclass
 class Context:
